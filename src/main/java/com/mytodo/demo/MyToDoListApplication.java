c8094d450eb111986268bfb61d932eae8651863c
trivediheena <hntrivedi72@gmail.com>
2023-03-09 20:47:39 +0530

    Updated Files

--- src/main/java/com/mytodo/demo/MyToDoListApplication.java
+++ src/main/java/com/mytodo/demo/MyToDoListApplication.java
@@ -2,10 +2,12 @@ package com.mytodo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan({"com.mytodo.demo.service","com.mytodo.demo.controller"})
+@EntityScan(basePackages = {"com.mytodo.demo.entities"})
 public class MyToDoListApplication {
 
 	public static void main(String[] args) {
